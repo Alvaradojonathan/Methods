bd234b42dc3608ed1e6e4451affe9e5e6bdf590a
Your Name <you@example.com>
2017-07-10 12:00:33 -0400

    method calling

--- methods/Program.cs
+++ methods/Program.cs
@@ -10,15 +10,56 @@ namespace methods
     {
         static void Main(string[] args)
         {
-            int answer = Add(3, 9);
-            Console.WriteLine(answer);
+            //Console.WriteLine("Please enter the first number to be added.");
+            //int numOne = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the second number to be added,");
+            //int numTwo = int.Parse(Console.ReadLine());
+            //int answer = Add(numTwo, numOne);
+            //Console.WriteLine(answer);
+
+            //RobotWarning("Will Robinson");
+            //Console.WriteLine("What's your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What's your favorite food?");
+            //string food = Console.ReadLine();
+            //FavoriteFood(name, food);
+
+            //Console.WriteLine("How old are you?");
+            //int age = int.Parse(Console.ReadLine());
+            //RetirementCalculator(age);
+
+            double hours = 42.3d;
+            double wage = 12.50d;
+
+            Console.WriteLine("Your monthly wage is {0}",WageCalculator(hours, wage));
         }
-        //Declaring a method = Method header and method body.
-        public static int Add(int firstNumber, int secondNumber)
+        ////Declaring a method = Method header and method body.
+        //public static int Add(int firstNumber, int secondNumber)
+        //{
+        //    int sum = firstNumber + secondNumber;
+        //    return sum;
+        //    //If a method does not return a value use the keyword "void".
+        //}
+        //public static void RobotWarning(string name)
+        //{
+        //    Console.WriteLine("Danger, " + name + "!!");
+        //}
+        //Create a method called favorite food
+        //public static void FavoriteFood(string name, string favoriteFood)
+        //{
+        //    Console.WriteLine(name + "'s favorite food is " + favoriteFood);
+
+        //}
+        //public static void RetirementCalculator(int userAge)
+        //{
+        //    int yearsToRetirment = 65 - userAge;
+        //    Console.WriteLine("The user will retire in {0} years.", yearsToRetirment);
+
+
+        public static double WageCalculator(double hoursWorkedWeekly, double hourlyWage)
         {
-            int sum = firstNumber + secondNumber;
-            return sum;
-            //If a method does not return a value use the keyword "void".
+            double monthlyWage = (hoursWorkedWeekly * 52 * hourlyWage) / 12;
+            return monthlyWage;
         }
     }
 }
